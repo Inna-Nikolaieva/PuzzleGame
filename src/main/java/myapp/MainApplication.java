e0abe9fbefde6bd3ab561cd910606b81f3cfdfef
Inna Nikolaieva <anastasiiainna@gmail.com>
2023-07-30 20:43:22 +0300

    Upload last version

--- src/main/java/myapp/MainApplication.java
+++ src/main/java/myapp/MainApplication.java
@@ -2,27 +2,9 @@ package myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
 
 @SpringBootApplication
 @RestController
@@ -35,7 +17,7 @@ public class MainApplication {
 
     @RequestMapping("/")
     public String index() {
-        return "index"; // This will serve the React app's HTML page
+        return "index";
     }
 }
 
